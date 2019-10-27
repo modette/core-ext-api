8149fbb44158fe814124424a2767bcef235fe80c
Marek Bartoš <bartos.developer152@gmail.com>
2019-10-27 15:20:01 +0100

    [Monorepo] Tests: Create Configurator in helper

---
+++ src/Controller.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace Modette\Api;
+
+use Apitte\Core\Annotation\Controller\Path;
+use Apitte\Core\UI\Controller\IController;
+
+/**
+ * @Path("/api")
+ */
+abstract class Controller implements IController
+{
+
+}
